5c4f0fe618acc7769fb4297d7dab93cb7e7f9d36
Terry Griffin <rugbyprof@users.noreply.github.com>
2017-01-22 18:48:04 -0600

    Update main.cpp

--- Lectures/Day-2/main.cpp
+++ Lectures/Day-2/main.cpp
@@ -4,12 +4,23 @@
 
 using namespace std;
 
+/**
+struct to hold a single rgb color
+*/
 struct rgb {
   int r;
   int g;
   int b;
 };
 
+/**
+* printColors
+* @params:
+*    rgb x[]: array of rgb colors
+*    int size: size of array
+* @returns: 
+*    void
+*/
 void printColors(rgb x[],int size){
   for(int i=0;i<size;i++){
     cout<<x[i].r<<","<<x[i].g<<","<<x[i].b<<endl;
@@ -17,27 +28,30 @@ void printColors(rgb x[],int size){
 }
 
 int main() {
-
-  ifstream ifile;
-  int r,g,b;
   
+  // file pointer
+  ifstream ifile;  
+
+  // array of rgb colors
   rgb colors[150];
   
+  // counter for colors
   int count = 0;
   
-  
+  // open color data file
   ifile.open("data.csv");
   
+  // read file until eof flag is set
   while(!ifile.eof()){
     ifile>>colors[count].r>>colors[count].g>>colors[count].b;
     count++;
   }
-
-  colors[count-1].g = 255;
-
   //here we know how many colors we have
   
-  printColors(colors,count);
+  // alter array for no reason except as an example on
+  // how to access a single color in an array of structs
+  colors[count-1].g = 255;
   
-
+  printColors(colors,count);
+ 
 }
