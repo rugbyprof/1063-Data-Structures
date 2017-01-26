ea33a2827acb262ff518f6b669ce33dbeb7b9949
Terry Griffin <rugbyprof@users.noreply.github.com>
2017-01-25 19:32:14 -0600

    Update starter.cpp

--- Assignments/02_Program_1/starter.cpp
+++ Assignments/02_Program_1/starter.cpp
@@ -1,9 +1,29 @@
+/**
+* TXT Image Manipulation Starter
+* 
+* This code is a simple way to read in color information stored in a space
+* delimited txt format. The expected file format is:
+*                ---------------------------
+*                | width height            |
+*                | R G B R G B R G B R G B |
+*                | R G B R G B R G B R G B |
+*                | R G B R G B R G B R G B |
+*                | R G B R G B R G B R G B |
+*                | R G B R G B R G B R G B |
+*                | R G B R G B R G B R G B |
+*                ---------------------------
+* So a 10x10 img would have 11 total rows, 10 rows of data, with 30 values in row.
+*/
+
 #include<iostream>
 #include<fstream>
 #include<math.h>
 
 using namespace std;
 
+/**
+Structure to hold an rgb value
+*/
 struct rgb{
     int r;
     int g;
@@ -11,33 +31,40 @@ struct rgb{
 };
 
 int main(){
-    ifstream ifile;
+    ifstream ifile;          //Input / output files
     ofstream ofile;
     ifile.open("bot.txt");
-    ofile.open("bot2.txt");
-    int width,height;
+    ofile.open("bot2.txt");   
     
+    int width;               //width of image
+    int height;              //height of image
     
-    rgb **img;
+    rgb **imgArray;         //Pointer var for our 2D array          
     
-    ifile>>width>>height;
+    ifile>>width>>height;   //Read in width and height from top of input file
+                            //We need this so we can make the array the right 
+                            //size.
     
-    img = new rgb*[height];
+    imgArray = new rgb*[height];    //This array points to every row
     
     for(int i=0;i<height;i++){
-        img[i] = new rgb[width*3];
+        imgArray[i] = new rgb[width]; //Now allocate each row of rgb's
     }
     
+    //Read the color data in from our txt file
     for(int i=0;i<height;i++){
         for(int j=0;j<width;j++){
-            ifile>>img[i][j].r>>img[i][j].g>>img[i][j].b;            
+            ifile>>imgArray[i][j].r>>imgArray[i][j].g>>imgArray[i][j].b;            
         }
     }
     
+    //We could make any changes we want to the color image here
+    
+    //Write out our color data to a new file
     ofile<<width<<" "<<height<<endl;
     for(int i=0;i<height;i++){
         for(int j=0;j<width;j++){
-            ofile<<img[i][j].r<<" "<<img[i][j].g<<" "<<img[i][j].b<<" ";
+            ofile<<imgArray[i][j].r<<" "<<imgArray[i][j].g<<" "<<imgArray[i][j].b<<" ";
         }
         ofile<<endl;
     }
