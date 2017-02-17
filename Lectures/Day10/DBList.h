710372bc9a578f5f93c4e1700aa8071c1976a292
Terry Griffin <rugbyprof@users.noreply.github.com>
2017-02-17 13:56:37 -0600

    Create DBList.h

---
+++ Lectures/Day10/DBList.h
@@ -0,0 +1,23 @@
+#include <iostream>
+
+struct Node{
+  int Data;
+  Node *Prev;
+  Node *Next;
+};
+
+class DBList{
+private:
+    Node *Head;
+    Node *Tail;
+    Node* _CreateNode(int);
+public:
+    DBList();
+    bool InsertFront(int);
+    bool InsertRear(int);
+    bool InsertInOrder(int);
+    bool Delete(int);   //Removes item from list
+    Node* Find(int);     //Locate and see if exists
+    bool Update(int,int);   //Change existing value
+    void Print();
+};
