30fea9b40002a7ff355286aeee2ad9f0e56fd99b
Terry Griffin <rugbyprof@users.noreply.github.com>
2018-01-31 11:29:48 -0600

    Update main.cpp

--- Lectures/L08/histogramProject/main.cpp
+++ Lectures/L08/histogramProject/main.cpp
@@ -1,1 +1,17 @@
-hello
+#include <iostream>
+#include <fstream>
+#include "wordHistogram.cpp"
+#include <string>
+
+using namespace std;
+
+
+int main() {
+  wordHistogram H;
+  wordNode biggest;
+  H.readFile("words.txt");
+  H.countWords();
+  biggest = H.Max();
+  
+  cout<<biggest.word<<":"<<biggest.count<<endl;
+}
