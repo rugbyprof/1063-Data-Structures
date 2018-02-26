0e95a0a2fd10177785db425ab31fa4889bf4bea5
Terry Griffin <rugbyprof@users.noreply.github.com>
2018-02-26 13:21:28 -0600

    Update list.cpp

--- Lectures/L12/list.cpp
+++ Lectures/L12/list.cpp
@@ -4,6 +4,8 @@
 
 using namespace std;
 
+// Struct to hold a word
+// and pointer to next word
 struct Node{
   string word;
   Node* next;
@@ -17,6 +19,15 @@ struct Node{
   }
 };
 
+/**
+* Class that represents a generic list with the following methods:
+*         List()                    // constructor to initialize 
+*   bool  Empty()                   // Is the list empty? (head == NULL)
+*   void  Push(string word)         // Pushes word to front of list
+*   void  PushBack(string word)     // Pushes word to end of list
+*   void  Print()                   // Prints entire list
+*   void  PrintRear()               // Prints value at rear of list
+*/
 class List{
 private:
   Node* Top;
@@ -24,11 +35,67 @@ private:
   int listSize;
   
 public:
+  /**
+  * Function: List()
+  * Description:
+  *    Initializes necessary data members.
+  * Params:
+  *    None
+  * Returns:
+  *    None
+  */
   List(){
     Top = Rear = NULL;
     listSize = 0;
   }
   
+  /**
+  * Function: Empty()
+  * Description:
+  *     Returns true if head == NULL
+  * Params:
+  *    None
+  * Returns:
+  *    bool [true,false]
+  */
+  bool Empty(){
+    return (Top == NULL);
+  }
+  
+  /**
+  * Function: Pop()
+  * Description:
+  *     Removes the first item from the list (item pointed to by head)
+  * Params:
+  *    None
+  * Returns:
+  *    string word
+  */
+  string Pop(){
+    if(Empty()){
+      return "error!";
+    }
+    string word;
+    
+    Node* temp = Top;
+    word = Top->word;
+    
+    Top = Top->next;
+    
+    delete temp;
+    
+    return word;
+  }
+  
+  /**
+  * Function: Push()
+  * Description:
+  *     Adds an item to the list (at the front closest to head).
+  * Params:
+  *    string word
+  * Returns:
+  *    void
+  */
   void Push(string word){
     Node* temp = new Node(word);
     
@@ -43,6 +110,15 @@ public:
     }
   }
   
+  /**
+  * Function: PushBack()
+  * Description:
+  *     Adds an item to the list (at the end using our rear pointer).
+  * Params:
+  *    string word
+  * Returns:
+  *    void
+  */
   void PushBack(string word){
     // Allocate new memory and initialize it
     Node* temp = new Node(word);
@@ -69,27 +145,7 @@ public:
       Top = Top->next;
     }
   }
-  
-  string Pop(){
-    if(Empty()){
-      return "error!";
-    }
-    string word;
-    
-    Node* temp = Top;
-    word = Top->word;
-    
-    Top = Top->next;
-    
-    delete temp;
-    
-    return word;
-  }
-  
-  bool Empty(){
-    return (Top == NULL);
-  }
-  
+ 
 };
 
 int main() {
@@ -100,30 +156,17 @@ int main() {
   string sch="";
   
   ifstream fin;
+ 
+  fin.open("animals.txt");
   
-  fin.open("notPalindrome.txt");
-  
-  fin.get(ch);
   while(!fin.eof()){
-    sch += ch;
-    L1.Push(sch);
-    sch = "";
-    fin.get(ch);
+    fin>>word;
+    L1.Push(word);
+    L2.PushBack(word);
   }
   
   L1.Print();
-  
-  // fin.open("animals.txt");
-  
-  // while(!fin.eof()){
-  //   fin>>word;
-  //   //cout<<word<<" ";
-  //   L1.Push(word);
-  //   L2.PushBack(word);
-  // }
-  
-  // L1.Print();
-  // cout<<endl;
-  // L2.Print();
+  cout<<endl;
+  L2.Print();
   
 }
