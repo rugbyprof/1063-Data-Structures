56d89af72b34131cd4d8582ea549b199d4b76695
rugbyprof <terry.griffin@mwsu.edu>
2019-08-28 19:23:48 -0500

    added some comments and more org on code

--- Lectures/L01/d1_arrays.cpp
+++ Lectures/L01/d1_arrays.cpp
@@ -11,7 +11,7 @@
 
 using namespace std;
 
-#define SIZE 10 // gives us a CONSTANT to use anywhere in our program 
+#define SIZE 10 // gives us a CONSTANT to use anywhere in our program \
                 // we capitalize constants so we know they are not variables!
 
 /**
@@ -29,16 +29,21 @@ using namespace std;
  */
 int countEvens(int *A, int size)
 {
-    int evens = 0;
+    int evens = 0;  // var to keep track of evens
 
+    // for loops work great when you know 
+    // when to stop (size of an array for example)
     for (int i = 0; i < size; i++)
     {
+        // num % 2 can only result in a `1` or a `0`
+        // where `1` => odd and `0` => even
         if (A[i] % 2 == 0)
         {
             evens++;
         }
     }
 
+    // return our even count
     return evens;
 }
 
@@ -57,15 +62,19 @@ int countEvens(int *A, int size)
  */
 int sumArray(int A[], int size)
 {
-    int total = 0;
+    int total = 0;  // variable to hold our total
+                    // sum vars need initialized!
 
+    // for loops great with arrays!
     for (int i = 0; i < size; i++)
     {
+        // keep our running total
         total = total + A[i];
     }
+
+    // return the total (sum)
     return total;
 }
-
 
 /**
  * Function: printArray
@@ -82,10 +91,14 @@ int sumArray(int A[], int size)
  */
 void printArray(int A[], int size)
 {
+    // for loops are great with _________________? 
     for (int i = 0; i < size; i++)
     {
+        // write out value with a space
         cout << A[i] << " ";
     }
+
+    // go to next line in output
     cout << endl;
 }
 
@@ -95,17 +108,30 @@ void printArray(int A[], int size)
 int main(int argc, char **argv)
 {
 
-    int A[SIZE];
+    int A[SIZE];    // declare integer array of SIZE (see CONSTANT at top of file)
 
-    for (int i = 0; i < SIZE; i++)
+    // Since for loops are good with arrays, lets
+    // mix it up a little bit!
+
+    int i = 0;  // my array index
+
+    // by boolean test: 
+    //      true  => keep looping
+    //      false => skip code block
+    while (i < SIZE)    
     {
         A[i] = rand() % 100;
+
+        // increment array counter every time
+        // if we don't loop will go FOREVER!!
+        i++;
     }
 
-    cout<<"Array Contents:";
+    // Printing our results to stdout
+    cout << "Array Contents:";
     printArray(A, SIZE);
 
-    cout <<"Sum: "<< sumArray(A, SIZE) << endl;
+    cout << "Sum: " << sumArray(A, SIZE) << endl;
 
-    cout <<"Evens: "<<countEvens(A, SIZE) << endl;
+    cout << "Evens: " << countEvens(A, SIZE) << endl;
 }
